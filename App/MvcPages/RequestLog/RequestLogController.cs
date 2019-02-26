343bc03ac23a544d9239c5d823decddd2553de6a
Alex <aldragan93@gmail.com>
2019-02-26 00:05:09 -0500

    Add search filtering to request log index

--- App/MvcPages/RequestLog/RequestLogController.cs
+++ App/MvcPages/RequestLog/RequestLogController.cs
@@ -1,6 +1,8 @@
 using System.Linq;
+using App.Util;
 using GenericServices;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using X.PagedList;
 
 namespace App.MvcPages.RequestLog
@@ -16,18 +18,55 @@ namespace App.MvcPages.RequestLog
 
 		public IActionResult Index(RequestLogIndexOptions options)
 		{
-			IPagedList<RequestLogIndexModel> pagedItems = _service.ReadManyNoTracked<RequestLogIndexModel>()
-				.OrderByDescending(i => i.RequestLogId)
-				.ToPagedList(options.CurrentPage, options.PageSize);
-
-
-			var y = (options.DetailType == RequestLogDetailType.All ? "selected" : "");
+			IQueryable<RequestLogIndexModel> items = _service.ReadManyNoTracked<RequestLogIndexModel>();
+			IPagedList<RequestLogIndexModel> pagedItems = IndexFilter(items, options);
 
 			return View(new IndexViewModel
 			{
 				Options = options,
 				PagedItems = pagedItems
 			});
+		}
+
+		//TODO: Move to ReuestLogService
+		private IPagedList<RequestLogIndexModel> IndexFilter(IQueryable<RequestLogIndexModel> model, RequestLogIndexOptions options)
+		{
+			if (options.RequestLogId != null)
+			{
+				model = model.Where(m => m.RequestLogId == options.RequestLogId);
+			}
+			if (options.StartDate != null)
+			{
+				model = model.Where(m => m.RequestBegin >= options.StartDate);
+			}
+			if (options.EndDate != null)
+			{
+				model = model.Where(m => m.RequestBegin <= options.EndDate);
+			}
+			if (options.InProgress)
+			{
+				model = model.Where(m => m.ResponseMs == null);
+			}
+			if (!options.URL.IsNullOrWhitespace())
+			{
+				model = model.Where(m => m.URL.Contains(options.URL));
+			}
+			if (!options.IP.IsNullOrWhitespace())
+			{
+				model = model.Where(m => m.IP.Contains(options.IP));
+			}
+			if (!options.RequestMethod.IsNullOrWhitespace())
+			{
+				model = model.Where(m => m.RequestMethod.Contains(options.RequestMethod));
+			}
+			if (options.ResponseStatus != null)
+			{
+				model = model.Where(m => m.ResponseStatus == options.ResponseStatus);
+			}
+
+			model = model.OrderByDescending(i => i.RequestLogId);
+			
+			return model.ToPagedList(options.CurrentPage, options.PageSize);
 		}
 
 		/*[HttpPost]
