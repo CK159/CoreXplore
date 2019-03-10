e14da939be27a9cd130d949e567a8f1af0cbc1d6
Alex <aldragan93@gmail.com>
2019-03-10 18:46:33 -0400

    Display current user roles and claims in non-core sample project

--- IdentityFramework/Controllers/HomeController.cs
+++ IdentityFramework/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,14 +11,31 @@ namespace IdentityFramework.Controllers
 	{
 		public ActionResult Index()
 		{
+			
+			
 			return View();
 		}
 
 		public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
+			
+			List<string> roles = ((ClaimsIdentity)User.Identity).Claims
+                            .Where(c => c.Type == ClaimTypes.Role)
+                            .Select(c => c.Value)
+                            .ToList();
 
-			return View();
+			return View(new AboutVM
+			{
+				Roles = roles,
+				Claims = ((ClaimsIdentity)User.Identity).Claims.ToList()
+			});
+		}
+
+		public class AboutVM
+		{
+			public List<string> Roles { get; set; }
+			public List<Claim> Claims { get; set; }
 		}
 
 		public ActionResult Contact()
@@ -25,6 +43,14 @@ namespace IdentityFramework.Controllers
 			ViewBag.Message = "Your contact page.";
 
 			return View();
+		}
+
+		[Authorize(Roles = "TestRole")]
+		public ActionResult TestRolePage()
+		{
+			ViewBag.Message = "This page requires 'TestRole' role.";
+
+			return View("Test");
 		}
 	}
 }
